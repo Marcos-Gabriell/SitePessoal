882de6ba4187dc5cf181ce621162600f7f5e5d58
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-06-04 15:59:22 -0300

    arrustando pagina de projetos

--- scripts2.js
+++ scripts2.js
@@ -1,31 +1,34 @@
-const ul = document.querySelector('ul')
+const ul = document.querySelector('ul');
 
 function getApiGitHub() {
   fetch('https://api.github.com/users/Marcos-Gabriell/repos')
     .then(async res => {
-
-      if(!res.ok) {
-        throw new Error(res.status)
+      if (!res.ok) {
+        throw new Error(res.status);
       }
 
-      var data = await res.json()
+      var data = await res.json();
 
       data.map(item => {
-        let li = document.createElement('li')
+        let li = document.createElement('li');
+        let repoLink = document.createElement('a');
+        repoLink.href = item.html_url;
+        repoLink.textContent = item.name.toUpperCase();
+        repoLink.target = '_blank'; // Abre o link em uma nova aba
 
-        li.innerHTML = `
-        <strong>${item.name.toUpperCase()}</strong>
-        <span>URL: ${item.url}</span>
-        <span>Data Criação: 
-          ${Intl.DateTimeFormat('pt-BR')
-            .format(new Date(item.created_at))}
-        </span>
-      `
-      ul.appendChild(li)
+        let repoInfo = document.createElement('span');
+        repoInfo.innerHTML = `
+          URL: ${item.html_url}<br>
+          Data Criação: ${Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}
+        `;
 
-      })
+        li.appendChild(repoLink);
+        li.appendChild(repoInfo);
 
-    }).catch(e => console.log(e))
+        ul.appendChild(li);
+      });
+    })
+    .catch(e => console.log(e));
 }
 
-getApiGitHub()
+getApiGitHub();
